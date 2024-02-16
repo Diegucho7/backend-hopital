c4efea4d8ac63772e41d95037ba0c3d6ab4d9e45
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-02-15 21:23:58 -0500

    apellidos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -11,7 +11,7 @@ const getUsuarios = async (req, res) =>{
     // const usuario = await Usuario.find({},'nombre apellido google email ');
     const [usuarios, total] = await Promise.all([
         Usuario
-        .find({},'nombre email role google img')
+        .find({},'nombre apellido email role google img')
         .skip(desde)
         .limit(5),
 
